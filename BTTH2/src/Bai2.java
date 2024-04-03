bd520bb4ef2c177403b58830d4031f58277df943
Pham Minh Triet <92496972+Nanome203@users.noreply.github.com>
2024-04-03 18:26:22 +0700

    Update Bai2.java

--- BTTH2/src/Bai2.java
+++ BTTH2/src/Bai2.java
@@ -21,51 +21,61 @@ public class Bai2 {
                         Math.pow(B.getY() - A.getY(), 2)));
 
         // 2
-
-        TamGiac test = new TamGiac(scanner);
-        test.nhap();
-        test.xuat();
-        test.tinhTien();
-        test.quay();
-        test.thuPhong();
-        System.out.println("\nChu vi tam giac: " + test.tinhChuVi());
-        System.out.println("\nDien tich tam giac: " + test.tinhDienTich());
-
-        TuGiac test2 = new TuGiac(scanner);
-        test2.nhap();
-        test2.xuat();
-        test2.tinhTien();
-        test2.quay();
-        test2.thuPhong();
-        System.out.println("\nChu vi tu giac: " + test2.tinhChuVi());
-        System.out.println("\nDien tich tu giac: " + test2.tinhDienTich());
-
-        HinhBinhHanh test3 = new HinhBinhHanh(scanner);
-        test3.nhap();
-        test3.xuat();
-        test3.tinhTien();
-        test3.quay();
-        test3.thuPhong();
-        System.out.println("\nChu vi hinh binh hanh: " + test3.tinhChuVi());
-        System.out.println("\nDien tich hinh binh hanh: " + test3.tinhDienTich());
-
-        HinhChuNhat test4 = new HinhChuNhat(scanner);
-        test4.nhap();
-        test4.xuat();
-        test4.tinhTien();
-        test4.quay();
-        test4.thuPhong();
-        System.out.println("\nChu vi hinh chu nhat: " + test4.tinhChuVi());
-        System.out.println("\nDien tich hinh chu nhat: " + test4.tinhDienTich());
-
-        HinhVuong test5 = new HinhVuong(scanner);
-        test5.nhap();
-        test5.xuat();
-        test5.tinhTien();
-        test5.quay();
-        test5.thuPhong();
-        System.out.println("\nChu vi hinh vuong: " + test5.tinhChuVi());
-        System.out.println("\nDien tich hinh chu nhat: " + test5.tinhDienTich());
+        System.out.print(
+                "Chon so tuong ung voi hinh muon nhap (1 - Tam Giac, 2 - Tu Giac, 3 - Hinh Binh Hanh, 4 - Hinh Chu Nhat, 5 - Hinh Vuong): ");
+        int selected = scanner.nextInt();
+        switch (selected) {
+            case 1:
+                TamGiac test = new TamGiac(scanner);
+                test.nhap();
+                test.xuat();
+                test.tinhTien();
+                test.quay();
+                test.thuPhong();
+                System.out.println("\nChu vi tam giac: " + test.tinhChuVi());
+                System.out.println("\nDien tich tam giac: " + test.tinhDienTich());
+                break;
+            case 2:
+                TuGiac test2 = new TuGiac(scanner);
+                test2.nhap();
+                test2.xuat();
+                test2.tinhTien();
+                test2.quay();
+                test2.thuPhong();
+                System.out.println("\nChu vi tu giac: " + test2.tinhChuVi());
+                System.out.println("\nDien tich tu giac: " + test2.tinhDienTich());
+                break;
+            case 3:
+                HinhBinhHanh test3 = new HinhBinhHanh(scanner);
+                test3.nhap();
+                test3.xuat();
+                test3.tinhTien();
+                test3.quay();
+                test3.thuPhong();
+                System.out.println("\nChu vi hinh binh hanh: " + test3.tinhChuVi());
+                System.out.println("\nDien tich hinh binh hanh: " + test3.tinhDienTich());
+                break;
+            case 4:
+                HinhChuNhat test4 = new HinhChuNhat(scanner);
+                test4.nhap();
+                test4.xuat();
+                test4.tinhTien();
+                test4.quay();
+                test4.thuPhong();
+                System.out.println("\nChu vi hinh chu nhat: " + test4.tinhChuVi());
+                System.out.println("\nDien tich hinh chu nhat: " + test4.tinhDienTich());
+                break;
+            case 5:
+                HinhVuong test5 = new HinhVuong(scanner);
+                test5.nhap();
+                test5.xuat();
+                test5.tinhTien();
+                test5.quay();
+                test5.thuPhong();
+                System.out.println("\nChu vi hinh vuong: " + test5.tinhChuVi());
+                System.out.println("\nDien tich hinh chu nhat: " + test5.tinhDienTich());
+                break;
+        }
 
         scanner.close();
     }
